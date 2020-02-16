6cf8cbcfd5dcd917dc35980eb7e4b8445ede11d3
linwenjun <linwenjun@163.com>
2020-02-16 15:57:19 +0000

    first commit

---
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.collection;
+
+import java.util.Collection;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println(randomNumber());
+    }
+
+    /**
+     *  生成10个0到20的随机整数，要求不能重复
+     */
+    public static Collection<Integer> randomNumber() {
+        return null;
+    }
+}
---
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.collection;
+
+import java.util.List;
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+        //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+    }
+
+    /**
+     * 创建一个List 然后在List中添加0到100整数
+     */
+    public static List<Integer> getNumbers() {
+        return null;
+    }
+
+
+}
