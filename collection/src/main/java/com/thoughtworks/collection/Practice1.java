3987dd30e3405d2f3e04c422552e7134e73debf4
zkj <438101845@qq.com>
2020-02-20 01:03:19 +0800

    [zkj]-course3-finished

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -11,6 +13,14 @@ public class Practice1 {
      *  生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        Collection<Integer> collection = new ArrayList<>();
+        Random random = new Random();
+        while (collection.size() < 10){
+            int num = random.nextInt(20);
+            if (!collection.contains(num)) {
+                collection.add(num);
+            }
+        }
+        return collection;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,18 +1,37 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        List<Integer> list = getNumbers();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).toString());
+        }
+
+        for (Integer i : list) {
+            System.out.println(i.toString());
+        }
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toString());
+        }
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
     public static List<Integer> getNumbers() {
-        return null;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 101; i++) {
+            list.add(i);
+        }
+        return list;
     }
 
 
